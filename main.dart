2bceefb8457e4ae7f10610a2948b6b4348da5058
CaioZanardo <caio_zanardo@hotmail.com>
2023-08-28 21:33:30 -0300

    Melhorando Codigo

--- main.dart
+++ main.dart
@@ -1,89 +1,104 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(CalculadoraApp());
-}
+void main() => runApp(MyApp());
 
-class CalculadoraApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calculadora de Multiplicação',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CalculadoraScreen(),
-    );
+    return TelaInicial();
   }
 }
 
-class CalculadoraScreen extends StatefulWidget {
+class TelaInicial extends StatefulWidget {
+  const TelaInicial({Key? key}) : super(key: key);
+
   @override
-  _CalculadoraScreenState createState() => _CalculadoraScreenState();
+  _TelaInicialState createState() => _TelaInicialState();
 }
 
-class _CalculadoraScreenState extends State<CalculadoraScreen> {
-  double numero1 = 0;
-  double numero2 = 0;
+class _TelaInicialState extends State<TelaInicial> {
+  TextEditingController num1Controller = TextEditingController();
+  TextEditingController num2Controller = TextEditingController();
   double resultado = 0;
+
+  void _calcular() {
+    double num1 = double.tryParse(num1Controller.text) ?? 0;
+    double num2 = double.tryParse(num2Controller.text) ?? 0;
+
+    setState(() {
+      resultado = num1 * num2;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de Multiplicação'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Mutiplicador de Numeros"),
+          centerTitle: true,
+        ),
+        body: _body(),
       ),
-      body: Container(
-        color: Colors.black, // Fundo preto
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: 'Digite o 1º Numero',
-                    labelStyle: TextStyle(color: Colors.white)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 25.0),
-                onChanged: (value) {
-                  setState(() {
-                    numero1 = double.tryParse(value) ?? 0;
-                  });
-                },
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: 'Digite o 2º Numero',
-                    labelStyle: TextStyle(color: Colors.white)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 25.0),
-                onChanged: (value) {
-                  setState(() {
-                    numero2 = double.tryParse(value) ?? 0;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    resultado = numero1 * numero2;
-                  });
-                },
-                child: Text('Calcular'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Resultado: $resultado',
-                style: TextStyle(
-                    fontSize: 25, color: Colors.white), // Texto branco
-              ),
-              SizedBox(height: 20),
-            ],
-          ),
+    );
+  }
+
+  _body() {
+    return Container(
+      width: double.infinity,
+      color: Colors.black,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _campo(num1Controller, "Digite o 1º número"),
+          _campo(num2Controller, "Digite o 2º número"),
+          _button(),
+          _resultado(),
+        ],
+      ),
+    );
+  }
+
+  _campo(TextEditingController controller, String labelText) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(color: Colors.white),
+      ),
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.white, fontSize: 25.0),
+      controller: controller,
+    );
+  }
+
+  _resultado() {
+    return Text(
+      "Resultado: $resultado",
+      style: TextStyle(fontSize: 25, color: Colors.white),
+    );
+  }
+
+  _button() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.blue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+      ),
+      onPressed: _calcular,
+      child: Text(
+        "Calcular",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
         ),
       ),
     );
